952ed597bd9be8a1b4a63b0286914e0f3b826ce2
Basile Henry <bjm.henry@gmail.com>
2017-07-15 12:55:31 +0100

    Using program with TEA (The ELm Architecture)

--- src/Main.elm
+++ src/Main.elm
@@ -1,17 +1,66 @@
 module Main exposing (..)
 
+import AnimationFrame exposing (diffs)
 import Collage exposing (..)
 import Color exposing (red)
 import Element exposing (toHtml)
-import Html exposing (Html)
+import Html exposing (Html, program)
+import Platform exposing (Program)
+import Time exposing (Time)
 
 
-main : Html msg
+type alias Model =
+    { x : Float
+    , y : Float
+    }
+
+
+type Msg
+    = Tick Time
+
+
+initialModel : Model
+initialModel =
+    { x = 0.0
+    , y = 0.0
+    }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ diffs Tick ]
+
+
+main : Program Never Model Msg
 main =
+    program
+        { init = ( initialModel, Cmd.none )
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+view : Model -> Html Msg
+view model =
     let
+        ball =
+            circle 30
+                |> filled red
+                |> moveX model.x
+                |> moveY model.y
+
         forms =
-            [ filled red (circle 50) ]
+            [ ball ]
     in
         forms
             |> collage 400 400
             |> toHtml
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Tick dt ->
+            ( { model | x = model.x + dt / 100 }, Cmd.none )
