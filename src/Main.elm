be4372526d668f3d5f1fe7e7da24d77f17466b87
Basile Henry <bjm.henry@gmail.com>
2017-07-15 11:55:46 +0100

    Using elm-graphics

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,17 @@
 module Main exposing (..)
 
-import Html exposing (Html, text)
+import Collage exposing (..)
+import Color exposing (red)
+import Element exposing (toHtml)
+import Html exposing (Html)
 
 
-main : Html a
+main : Html msg
 main =
-    text "Hello, World!"
+    let
+        forms =
+            [ filled red (circle 50) ]
+    in
+        forms
+            |> collage 400 400
+            |> toHtml
